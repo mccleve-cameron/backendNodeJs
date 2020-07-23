a1c1a4bc5785234f72587585a6b736eaf42e4ff4
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-07-23 12:11:55 -0600

    bodyBuild

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const connectionString = process.env.DATABASE_URL || 'postgres://wbtddyzaexpzji:
 
 const pool = new Pool({connectionString: connectionString});
 
+let muscle = 'chest';
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 express()
@@ -16,80 +18,38 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/mail', (req, res) => res.render('pages/mail'))
-  .get('/receipt', (req, res) => {
-    const weight = req.query.iWeight;
-    const mailType = req.query.mail;
-    const shipCost = req.query.shipCost;
-    //req.body
-
-    const cost = calcRate(mailType, weight);
-
-    param = { weight: weight, mailType: mailType, cost: cost}
-    res.render('pages/receipt', param)
-  })
-  .get('/users', (req, res) => res.render('pages/users'))
-  .post('/getUsers', getUsers)
-
-  .get('/bodyMap', (req, res) => res.render('pages/bodyMap'))
-  .get('/chest', (req, res) => res.render('pages/chest'))
-  // .get('/chest', (req, res) => {
-  //   const name = req.query.
+  // .get('/mail', (req, res) => res.render('pages/mail'))
+  // .get('/receipt', (req, res) => {
+  //   const weight = req.query.iWeight;
+  //   const mailType = req.query.mail;
+  //   const shipCost = req.query.shipCost;
+  //   //req.body
 
   //   const cost = calcRate(mailType, weight);
 
   //   param = { weight: weight, mailType: mailType, cost: cost}
   //   res.render('pages/receipt', param)
   // })
-  .post('/getWorkouts', getWorkouts)
-  // .get('/workout', getWorkout)
-  // .post('/getDescription', getDescription)
+  .get('/users', (req, res) => res.render('pages/users'))
+  .post('/getUsers', getUsers)
+
+  .get('/bodyMap', (req, res) => res.render('pages/bodyMap'))
+  
+  .get('/displayExerciseList', (req, res) => res.render('pages/displayExerciseList'))
+  .post('/getExerciseList', getExerciseList)
+  
+  .get('/displayExerciseDetails', (req, res) => res.render('pages/displayExerciseDetails'))
+  .post('/getExerciseDetails', getExerciseDetails)
 
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-//   function getWorkout(request, response) {
-//  const name = request.query.workout;
-
-//     queryWorkout(name, function(error, result) {
-
-//       if (error || result == null || result.length == 0) {
-//         response.status(500).json({success: false, data: error});
-//       } else {
-//         const workout = result;
-//         response.status(200).json(workout);
-//       }
-//     });
-//   }
-
-//   function queryWorkout(name, callback) {
-//     console.log(name);
-
-//     const sql = "SELECT * FROM chest WHERE name= $1::text";
-
-//     params = [name];
-  
-
-//     pool.query(sql, params, function(err, result) {
-//       // If an error occurred...
-//       if (err) {
-//         console.log("Error in query: ")
-//         console.log(err);
-//         callback(err, null);
-//       }
-  
-//       // Log this to the console for debugging purposes.
-//       console.log("Found result: " + JSON.stringify(result.rows));
-
-//       callback(null, result.rows);
-//     });
-  
-//   }
-
-
-  function getWorkouts(request, response) {
- 
-    queryWorkouts(function(error, result) {
+  function getExerciseDetails(request, response) {
+    
+    const name = request.body.workout;
+    console.log(name);
+    console.log(muscle);
+    queryExerciseDetails(name, function(error, result) {
 
       if (error || result == null || result.length == 0) {
         response.status(500).json({success: false, data: error});
@@ -100,10 +60,51 @@ express()
     });
   }
 
-  function queryWorkouts(callback) {
-    console.log("Getting person from DB with id: ");
+  function queryExerciseDetails(name, callback) {
+    console.log(name);
 
-    const sql = "SELECT * FROM chest";
+    const sql = "SELECT * FROM "+muscle+" WHERE id=" + name;
+    console.log(sql);
+  
+
+    pool.query(sql, function(err, result) {
+      // If an error occurred...
+      if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+        callback(err, null);
+      }
+  
+      // Log this to the console for debugging purposes.
+      console.log("Found result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+    });
+  
+  }
+
+
+  function getExerciseList(request, response) {
+ 
+    muscle = request.body.muscle;
+    console.log(muscle);
+    queryWorkouts(muscle, function(error, result) {
+
+      if (error || result == null || result.length == 0) {
+        response.status(500).json({success: false, data: error});
+      } else {
+        const workout = result;
+        response.status(200).json(workout);
+      }
+    });
+  }
+
+  function queryWorkouts(muscle, callback) {
+    console.log(muscle);
+
+    const sql = "SELECT * FROM " +muscle;
+   // param = [muscle];
+    console.log(sql);
 
     pool.query(sql, function(err, result) {
       // If an error occurred...
@@ -122,17 +123,7 @@ express()
   }
 
   function getUsers(request, response) {
-    // First get the person's id
-    //const id = request.query.id;
-  
-    // TODO: We should really check here for a valid id before continuing on...
-  
-    // use a helper function to query the DB, and provide a callback for when it's done
     queryData(function(error, result) {
-      // This is the callback function that will be called when the DB is done.
-      // The job here is just to send it back.
-  
-      // Make sure we got a row with the person, then prepare JSON to send back
       if (error || result == null || result.length == 0) {
         response.status(500).json({success: false, data: error});
       } else {
@@ -144,17 +135,8 @@ express()
 
   function queryData(callback) {
     console.log("Getting person from DB with id: ");
-  
-    // Set up the SQL that we will use for our query. Note that we can make
-    // use of parameter placeholders just like with PHP's PDO.
     const sql = "SELECT * FROM users";
-  
-    // We now set up an array of all the parameters we will pass to fill the
-    // placeholder spots we left in the query.
-    //const params = [id];
-  
-    // This runs the query, and then calls the provided anonymous callback function
-    // with the results.
+
     pool.query(sql, function(err, result) {
       // If an error occurred...
       if (err) {
@@ -162,102 +144,14 @@ express()
         console.log(err);
         callback(err, null);
       }
-  
-      // Log this to the console for debugging purposes.
       console.log("Found result: " + JSON.stringify(result.rows));
-  
-  
-      // When someone else called this function, they supplied the function
-      // they wanted called when we were all done. Call that function now
-      // and pass it the results.
-  
-      // (The first parameter is the error variable, so we will pass null.)
+
       callback(null, result.rows);
     });
   
-  } // end of getPersonFromDb
-
-
-
-
-  function calcRate(mailType, weight) {
-    switch (mailType) {
-      case "Stamped Letter":
-        return stamped(weight);
-      case "Metered Letter":
-        return metered(weight);
-      case "Envelope":
-        return envelope(weight);
-      case "Package":
-        return package(weight);
-      default:
-        return -1;
-    }
   }
 
-  function stamped(weight) {
-		console.log("stamped called");
 
-		if (weight > 0 && weight < 1) {
-			shipCost = .55;
-			console.log(shipCost);
-		}
-		if (weight >= 1 && weight < 2) {
-			console.log('.70');
-			shipCost = .70;
-		}
-		if (weight >= 2 && weight < 3) {
-			shipCost = .85;
-			console.log('.85');
-		}
-		if (weight >= 3 && weight < 3.5) {
-			shipCost = 1.00;
-			console.log('1.00');
-    }
-    return shipCost;
-  }
-  
-  function metered(weight) {
-		console.log("metered called");
-		
-		if (weight > 0 && weight < 1) {
-			shipCost = .50;
-		}
-		if (weight >= 1 && weight < 2) {
-			shipCost = .65;
-		}
-		if (weight >= 2 && weight < 3) {
-			shipCost = .80;
-		}
-		if (weight >= 3 && weight < 3.5) {
-			shipCost = .95;
-    }
-    return shipCost;
-  }
-  
-  function envelope(weight) {
-    console.log("envelope called");
-    let tmp = weight - 1;
 
-    shipCost = tmp * .2 + 1;
-	
-    return shipCost;
-  }
-  
-  function package(weight) {
-		console.log("package called");
-		
-		if (weight > 0 && weight <= 4) {
-			shipCost = 3.8;
-		}
-		if (weight > 4 && weight <= 8) {
-			shipCost = 4.6;
-		}
-		if (weight > 8 && weight <= 12) {
-			shipCost = 5.3;
-		}
-		if (weight > 12) {
-			shipCost = 5.9;
-    }
-    return shipCost;
-  }
+
+ 
